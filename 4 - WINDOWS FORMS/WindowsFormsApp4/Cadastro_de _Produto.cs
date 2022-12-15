d8f3d94da4cc7e158152f36bbe01bdca1d5a22ff
Gabriel <castilhogabriel430@gmail.com>
2022-12-15 20:53:12 -0300

    Alteração para Cadastro de Produto

---
+++ 4 - WINDOWS FORMS/WindowsFormsApp4/Cadastro_de _Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    public class Cadastro_de_Produto
+    {
+        public int Id { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Cadastro-de-Produto
+realizei uma alteração no programa Windows Forms para o Cadastro de  Produto. 
